57c12157dd945a9cccdbb88ec44717c801c2be47
thevoyagertwo <thevoyagertwo@protonmail.com>
2021-08-24 17:31:19 +0100

    Added data, and reading file. Last step is to fix ranges for inserting coordinates into np.array And having first and last coord be the same for nice looping

--- main.py
+++ main.py
@@ -1,12 +1,61 @@
+
+
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import numpy as np
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# maybe store the first point as the end point as well to lead to nicer loops when testing
+def data_extract(filename):
+
+
+    file = open("data.txt", "r")
+
+        # data output : ['OUTLINE 8', '2100 19640', '29255 19640', '29255 1125', '20450 1125', '20450 8345',
+    raw_data = file.read().splitlines()
+    print(raw_data[18].split(" "))
+
+
+    outline_data = np.zeros((len(raw_data),2))
+
+    # works but need to add ranges for the data, as does not handle strings.
+    for i in range(1 , len(raw_data)):
+
+        temp_row = raw_data[i].split(" ")
+        outline_data[i,0] = temp_row[0]
+        outline_data[i,1] = temp_row[1]
+
+
+
+
+
+
+    # full_data(0,0) = 10
+
+
+    # for row in file:
+    #     file_data = row.split(" ")
+    #
+    #
+    #     print(file_data[0])
+    #     print(file_data[1])
+
+    file.close()
+
+
+    return outline_data
+
+
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    filename = 'data.txt'
+
+    file_data = data_extract(filename)
+    print(file_data)
+
+
+
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
